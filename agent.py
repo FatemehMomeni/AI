6117261fec9b8c2c6b50e446b425fb2f3c11bcc8
Finemom <momeni.ui.ac@gmail.com>
2021-05-26 16:45:54 +0430

    'new_approach'

--- agent.py
+++ agent.py
@@ -1,36 +1,43 @@
-import random
+import operator
 from base import BaseAgent, TurnData, Action
+from tree import A_star
+
+
+turn_counter = 0
+diamond_pos = list(tuple())
+bases_pos = list(tuple())
+bases_cost = list(tuple())
+result = list()
 
 
 class Agent(BaseAgent):
 
     def do_turn(self, turn_data: TurnData) -> Action:
-        if turn_data.turns_left == self.max_turns:
-            print(f"MY NAME: {self.name}")
-            print(f"PLAYER COUNT: {self.agent_count}")
-            print(f"GRID SIZE: {self.grid_size}")
-            print(f"MAX TURNS: {self.max_turns}")
-            print(f"DECISION TIME LIMIT: {self.decision_time_limit}")
-        print(f"TURN {self.max_turns - turn_data.turns_left}/{self.max_turns}")
-        for agent in turn_data.agent_data:
-            print(f"AGENT {agent.name}")
-            print(f"POSITION: {agent.position}")
-            print(f"CARRYING: {agent.carrying}")
-            print(f"COLLECTED: {agent.collected}")
-            print(f"SCORE: {agent.score}")
-            print(f"REQUIRED: {agent.count_required}")
-        for row in turn_data.map:
-            print(''.join(row))
-        action_name = input("> ").upper()
-        if action_name == "U":
-            return Action.UP
-        if action_name == "D":
-            return Action.DOWN
-        if action_name == "L":
-            return Action.LEFT
-        if action_name == "R":
-            return Action.RIGHT
-        return random.choice(list(Action))
+        global turn_counter, diamond_pos, bases_pos, result, bases_cost
+
+        if turn_counter == 0:
+            for i in range(self.grid_size-1):
+                if turn_data.map[i] == r"\d":
+                    diamond_pos.append((i / self.grid_size, i % self.grid_size))
+                elif turn_data.map[i] == r"[a-z]":
+                    bases_pos.append((i / self.grid_size, i % self.grid_size))
+            turn_counter += 1
+
+            for agent in turn_data.agent_data:
+                algorithm = A_star(self.grid_size, agent.position, diamond_pos, turn_data.map)
+                path, cost = algorithm.solution(agent.position)
+                result = path
+
+            for agent in turn_data.agent_data:
+                for base in range(len(bases_pos)):
+                    algorithm = A_star(self.grid_size, agent.position, bases_pos[base], turn_data.map)
+                    path, cost = algorithm.solution(agent.position)
+                    bases_cost.append([path, cost])
+            min_path = min(bases_cost, key=lambda t: t[1])
+            result.append(min_path[0])
+
+        current_act = result.pop(0)
+        return current_act
 
 
 if __name__ == '__main__':
--- tree.py
+++ tree.py
@@ -1,59 +1,95 @@
-frontier = []
-explored = []
-
-
 class Node:
 
-    def __init__(self, position, parent):
-        self.position = position ## root
-        #self.children = []
+    def __init__(self, position, parent, cost, action):
+        self.position = position
         self.parent = parent
+        self.cost = cost
+        self.action = action
 
 
-class a:
-    destination = None
+frontier = list[Node]
+explored = list[Node]
+path = list()
 
-    def __init__(self, grid_size, position, goal_position):
+
+class A_star:
+    #destination = None
+
+    def __init__(self, grid_size, position, goal_position, mapp):
         self.grid_size = grid_size
         self.position = position
         self.destination = goal_position
-        self.expand(self.position)
+        self.map = mapp
 
-    def expand(self, position):
-        actions = [(position[0]-1, position[1]), (position[0]+1, position[1]),
-                   (position[0], position[1]-1), (position[0], position[1]+1)]
+    def expand(self, pos):
+        actions = {"UP": (pos[0]-1, pos[1]), "DOWN": (pos[0]+1, pos[1]),
+                   "LEFT": (pos[0], pos[1]-1), "RIGHT": (pos[0], pos[1]+1)}
+
         if not frontier:
-            cost = self.f_n(position, self.destination)
-            frontier.append([position, cost])
-            root = Node(position, None)
+            price = self.f_n(None, pos, self.destination)
+            frontier.append([pos, None, price, None])
         else:
-            if position[0] == 0:
-                actions.pop(0)
-            elif position[0] == self.grid_size - 1:
-                actions.pop(1)
-            if position[1] == 0:
-                actions.pop(2)
-            elif position[1] == self.grid_size - 1:
-                actions.pop(3)
-        for act in actions:
-            if act != '*':
-                if act not in explored:
-                    cost = self.f_n(act, self.destination)
-                    node = Node(act, position)
-                    if act not in frontier:
-                        frontier.append([act, cost])
-                    else:
-                        i = frontier.index(act)
-                        frontier[i][1] = cost
-        min = frontier[0][1]
-        for i in range(len(frontier)):
-            if frontier[i][1] < min:
-                self.expand(frontier[i][0])
-                frontier.pop(i)
+            if pos[0] == 0:
+                actions["UP"] = (0, 0)
+            elif pos[0] == self.grid_size - 1:
+                actions["DOWN"] = (0, 0)
+            if pos[1] == 0:
+                actions["LEFT"] = (0, 0)
+            elif pos[1] == self.grid_size - 1:
+                actions["RIGHT"] = (0, 0)
 
-    def f_n(self, position, goal_position):
-        # add cost of parents in g_n       "REMEMBER"
-        i = frontier.index(position)
-        g_n = 1 + frontier[i][1]
+        explored_flag = False
+        flag = True
+        minimum = 0
+        min_index = 0
+        for act_key, act_value in actions.items():
+            #global flag, minimum, min_index, explored_flag
+            if act_value != (0, 0) and self.map[self.grid_size * act_value[0] + act_value[1]] != '*':
+                for ex_node in explored:
+                    if act_value == ex_node.position:
+                        explored_flag = True
+                if not explored_flag:
+                    price = self.f_n(pos, act_value, self.destination)
+                    for node in frontier:
+                        if min_index == 0:
+                            minimum = node.cost
+                            min_index = 1
+                        if node.position == act_value and node.cost > price:
+                            node.cost = price
+                            node.parent = pos
+                            node.action = act_key
+                            flag = False
+                            break
+                    if flag:
+                        frontier.append([act_value, pos, price, act_key])
+
+        for node in frontier:
+            if node.cost < minimum:
+                minimum = node.cost
+        for node in frontier:
+            if node.cost == minimum:
+                if node.position == self.destination:
+                    return node.position, node.cost
+                else:
+                    self.expand(node.position)
+                    frontier.pop(node)
+                    explored.append([node.position, node.parent, node.cost, node.action])
+                    break
+
+    def f_n(self, parent, position, goal_position):
+        global costt
+        costt = 0
+        for node in frontier:
+            if node.position == parent:
+                costt = node.cost
+        g_n = 1 + costt
         h_n = (abs(goal_position[0] - position[0])) + (abs(goal_position[1] - position[1]))
         return g_n + h_n
+
+    def solution(self, pos):
+        goal_pos, goal_cost = self.expand(pos)
+        for ex in explored:
+            if ex.position == goal_pos:
+                path.index(0, ex.action)
+                goal_pos = ex.parent
+        return path, goal_cost
