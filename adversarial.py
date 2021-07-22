7377eeda5efe146f9e6f3ffe0c45ad8882fa7cda
Finemom <momeni.ui.ac@gmail.com>
2021-07-22 17:48:50 +0430

    'finally....worked'

--- adversarial.py
+++ adversarial.py
@@ -7,57 +7,66 @@ tree_container: Dict[Any, Any] = dict()
 
 class Minimax_tree:
 
-    def __init__(self, id, parent_id, turn: int, agent_num, utility, collect):
+    def __init__(self, id, parent_id, unique, turn: int, agent_num, utility, collect):
         # collect: collected diamond for each agent
         self.agent_num = agent_num
         self.id = id
         self.parent_id = parent_id
+        self.unique = unique
         self.turn = turn
         self.utility = utility
-        self.agents_collected = [[] for _ in range(self.agent_num)]
+        self.agents_collected = [{0: [], 1: [], 2: [], 3: [], 4: []} for _ in range(self.agent_num)]
         for arg in range(len(collect)):
-            self.agents_collected[arg].extend(collect[arg])
+            self.agents_collected[arg] = collect[arg]
 
     @staticmethod
-    def search(id, parent_id, turn):
+    def search_unique(id, parent_id, turn):
         for obj in tree_container.values():
             for v in obj:
                 if v.id == id and v.parent_id == parent_id and v.turn == turn:
-                    return v.utility
+                    return v.unique
         return None
 
     @staticmethod
-    def search_collected(id, parent_id, turn):
+    def search_collected_utility(id, parent_id, turn):
         for obj in tree_container.values():
             for v in obj:
                 if v.id == id and v.parent_id == parent_id and v.turn == turn:
-                    return v.agents_collected
+                    return v.agents_collected, v.utility
         return None
 
     @staticmethod
-    def search_util_turn(id, parent_id):
+    def search_turn(id):
         for obj in tree_container.values():
             for v in obj:
-                if v.id == id and v.parent_id == parent_id:
-                    return v.utility, v.turn
+                if v.unique == id:
+                    return v.turn
         return None
 
     @staticmethod
-    def search_parent(id):
+    def search_pos(id):
         for obj in tree_container.values():
             for v in obj:
-                if v.id == id:
-                    return v.parent_id
+                if v.unique == id:
+                    return v.id
         return None
+
+    @staticmethod
+    def update_utility(id, utility: list):
+        for obj in tree_container.values():
+            for v in obj:
+                if v.unique == id:
+                    v.utility = utility
 
 
 class Minimax_algorithm:
     diamond_scores = [2, 5, 3, 1, 10]  # green, blue, red, yellow, gray
+    unique = 0
 
     def __init__(self, diamonds: dict, min_required: dict, pos_A):
         self.diamond_num = list()
         self.utility = list()
-        self.collected: list[any, any] = list()
+        self.collected: dict[any, any] = dict()
         self.min_required = min_required
         self.agent_num = len(list(self.min_required))
         self.diamonds = diamonds
@@ -68,47 +77,57 @@ class Minimax_algorithm:
     def minimax(self, position: tuple, parent: tuple, turn: int, current_diamonds: dict):
         pos = par = tuple()
         children_pos = list()
-        real_collected = list()
+        real_collected = dict()
+        real_utility = list()
 
         for color in range(len(self.diamond_num)):
+            par_unique = 0
+            if turn != 0:
+                par_turn = turn - 1
+            else:
+                par_turn = self.agent_num - 1
             if parent == (-1, -1):
-                self.collected = [[] for _ in range(self.agent_num)]
+                self.collected = [{0: [], 1: [], 2: [], 3: [], 4: []} for _ in range(self.agent_num)]
                 self.utility = [0 for _ in range(self.agent_num)]
-                tree_container[(-1, -1)] = [Minimax_tree(position, (-1, -1), turn, self.agent_num,
-                                                         self.utility, self.collected)]
+                #tree_container[((-1, -1), -1)] = [Minimax_tree(position, (-1, -1), self.unique, turn,
+                                                               #self.agent_num, self.utility, self.collected)]
             elif color == 0:
-                self.collected = Minimax_tree.search_collected(position, parent, not turn)
+                self.collected, self.utility = Minimax_tree.search_collected_utility(position, parent, par_turn)
                 real_collected = deepcopy(self.collected)
+                real_utility = deepcopy(self.utility)
             else:
                 self.collected = deepcopy(real_collected)
+                self.utility = deepcopy(real_utility)
             if len(current_diamonds[color]) != 0:
                 if parent != (-1, -1):
-                    self.utility = Minimax_tree.search(position, parent, not turn)
+                    par_unique = Minimax_tree.search_unique(position, parent, par_turn)
                 color_num = random.randint(0, len(current_diamonds[color])-1)
-                self.collected[turn].append(current_diamonds[color][color_num])
+                self.collected[turn][color].append(current_diamonds[color][color_num])
+                if len(self.collected[turn][color]) >= self.min_required[turn][color]:
+                    self.utility[turn] += self.diamond_scores[color]
                 locals()
-                current = dict()
+                current = {0: [], 1: [], 2: [], 3: [], 4: []}
                 for k, v in current_diamonds.items():
                     if v:
                         for i in range(len(v)):
-                            flag = True
-                            for c in self.collected:
-                                if v[i] in c:
-                                    flag = False
+                            not_selected = True
+                            for c in range(self.agent_num):
+                                if v[i] in self.collected[c][k]:
+                                    not_selected = False
                                     break
-                            if flag:
-                                current.setdefault(k, []).append(v[i])
-                            else:
-                                current.setdefault(k, [])
-                    else:
-                        current.setdefault(k, [])
+                            if not_selected:
+                                current[k].append(v[i])
+
                 pos = current_diamonds[color][color_num]
                 children_pos.extend([[pos, current]])
                 par = position
-                if self.collected[turn].count(color) >= self.min_required[turn][color]:
-                    self.utility[turn] += self.diamond_scores[color]
-                node = Minimax_tree(pos, par, turn, self.agent_num, self.utility, self.collected)
-                tree_container.setdefault(par, []).append(node)
+
+                """print('pos', pos, 'par', par, 'collected', self.collected, 'unique', self.unique, 'par_unique', par_unique,
+                      'utility', self.utility, 'turn', turn, '\n---------------------------------------------------')"""
+
+                self.unique += 1
+                node = Minimax_tree(pos, par, self.unique, turn, self.agent_num, self.utility, self.collected)
+                tree_container.setdefault(par_unique, []).append(node)
 
         if turn % self.agent_num == self.agent_num - 1:
             turn = 0
@@ -122,33 +141,40 @@ class Minimax_algorithm:
         return
 
     def sequence(self):
-        self.find_best_child(tree_container.get((-1, -1)))
-        sequence = list()  # last element of this list is root of the found path, so it should be parsed from end
-        print(tree_container)
-        #print('sequence', sequence)
-        sequence.append(list(tree_container.values())[0][1])
-        end_sequence = False
-        while not end_sequence:
-            node = Minimax_tree.search_parent(sequence[0])
-            sequence.insert(0, node)
-            if node is None:
-                end_sequence = True
+        self.find_best_child(0, tree_container.get(0))
+        sequence = list()
+        path = 0
+        while path in tree_container:
+            sequence.append(tree_container[path][0].unique)
+            path = tree_container[path][0].unique
+
+        for i in range(len(sequence)):
+            sequence[i] = Minimax_tree.search_pos(sequence[i])
+
         return sequence
 
-    def find_best_child(self, children: list):
+    def find_best_child(self, parent: int, children: list):
         max_list = dict()
+        maximum = 0
+        utility = list()
         for child in children:
-            max_list.clear()
-            maximum = 0
-            if child in tree_container:
-                self.find_best_child(tree_container[child])
+            if child.unique in tree_container:
+                self.find_best_child(child.unique, tree_container[child.unique])
+            utility_vector = child.utility
+            if parent:
+                turn = Minimax_tree.search_turn(parent)
+                if turn != self.agent_num - 1:
+                    turn += 1
+                else:
+                    turn = 0
             else:
-                utility_vector, turn = Minimax_tree.search_util_turn(child.id, child.parent_id)
-                parent = Minimax_tree.search_parent(child.id)
-                if utility_vector[turn] > maximum:
-                    locals()
-                    maximum = utility_vector[turn]
-                    max_list.clear()
-                    max_list[parent] = [utility_vector, child]
+                turn = 0
+            if utility_vector[turn] > maximum:
+                locals()
+                maximum = utility_vector[turn]
+                max_list.clear()
+                max_list[parent] = [child]
+                utility = utility_vector
 
+        Minimax_tree.update_utility(parent, utility)
         tree_container.update(max_list)
--- agent.py
+++ agent.py
@@ -1,4 +1,4 @@
-from base import BaseAgent, TurnData, Action, AgentData
+from base import BaseAgent, TurnData, Action
 from tree import A_star
 from adversarial import Minimax_algorithm
 import re
@@ -14,67 +14,84 @@ result = list()
 # PHASE 4
 diamond_pos_4 = {0: [], 1: [], 2: [], 3: [], 4: []}
 bases_pos_4 = {'a': [], 'b': [], 'c': [], 'd': []}
-result_4 = list(list())
-agent_turn = 0
+required = dict()
+result_4 = list()
+pos_A = tuple()
 
 
 class Agent(BaseAgent):
 
     def do_turn(self, turn_data: TurnData) -> Action:
-        global turn_counter, diamond_pos, bases_pos, result, bases_cost, diamond_pos_4, bases_pos_4,\
-            agent_turn, result_4
+        global turn_counter, diamond_pos_4, bases_pos_4, result_4, required, pos_A
 
         # ------------------------------------------ PHASE 4 ------------------------------------------
-        if turn_counter == 0:
+        if turn_counter == 0:  # first time
             for map_row in range(self.grid_size):
                 for map_column in range(self.grid_size):
-                    a = turn_data.map[map_row][map_column]
                     if re.match(r'\d', turn_data.map[map_row][map_column]):
                         if turn_data.map[map_row][map_column] == '0':
-                            diamond_pos_4[0].append((map_row, map_column))
+                            diamond_pos_4.setdefault(0, []).append((map_row, map_column))
                         elif turn_data.map[map_row][map_column] == '1':
-                            diamond_pos_4[1].append((map_row, map_column))
+                            diamond_pos_4.setdefault(1, []).append((map_row, map_column))
                         elif turn_data.map[map_row][map_column] == '2':
-                            diamond_pos_4[2].append((map_row, map_column))
+                            diamond_pos_4.setdefault(2, []).append((map_row, map_column))
                         elif turn_data.map[map_row][map_column] == '3':
-                            diamond_pos_4[3].append((map_row, map_column))
+                            diamond_pos_4.setdefault(3, []).append((map_row, map_column))
                         else:
-                            diamond_pos_4[4].append((map_row, map_column))
+                            diamond_pos_4.setdefault(4, []).append((map_row, map_column))
                     elif re.match(r'[a-d]', turn_data.map[map_row][map_column]):
                         if turn_data.map[map_row][map_column] == 'a':
-                            bases_pos_4['a'].append((map_row, map_column))
+                            bases_pos_4.setdefault('a', []).append((map_row, map_column))
                         elif turn_data.map[map_row][map_column] == 'b':
-                            bases_pos_4['b'].append((map_row, map_column))
+                            bases_pos_4.setdefault('b', []).append((map_row, map_column))
                         elif turn_data.map[map_row][map_column] == 'c':
-                            bases_pos_4['c'].append((map_row, map_column))
+                            bases_pos_4.setdefault('c', []).append((map_row, map_column))
                         else:
-                            bases_pos_4['d'].append((map_row, map_column))
+                            bases_pos_4.setdefault('d', []).append((map_row, map_column))
             turn_counter += 1
 
-            num = -1
             for agent in range(len(turn_data.agent_data)):
-                if turn_data.agent_data[agent].name == self.name:
-                    num = agent
-                    break
-            sequence = Minimax_algorithm(diamond_pos_4, turn_data.agent_data[num].count_required, len(turn_data.agent_data))
+                if agent == 0:
+                    required.setdefault(0, []).extend(turn_data.agent_data[agent].count_required)
+                    pos_A = turn_data.agent_data[agent].position
+                elif agent == 1:
+                    required.setdefault(1, []).extend(turn_data.agent_data[agent].count_required)
+                elif agent == 2:
+                    required.setdefault(2, []).extend(turn_data.agent_data[agent].count_required)
+                else:
+                    required.setdefault(3, []).extend(turn_data.agent_data[agent].count_required)
 
-            origin_pos = turn_data.agent_data[num]
-            while sequence.answer:
-                for diamond in sequence.answer:
-                    algorithm = A_star(self.grid_size, origin_pos, diamond, turn_data.map)
-                    d_path, d_cost = algorithm.solution(origin_pos)
-                    for base in bases_pos:
-                        algorithm = A_star(self.grid_size, d_path, base, turn_data.map)
-                        b_path, b_cost = algorithm.solution(d_path)
-                        bases_cost[base] = [b_path, b_cost]
-                    min_base = min(bases_cost.keys(), key=lambda t: bases_cost[t][1])
-                    min_b_path = bases_cost[min_base][0]
-                    origin_pos = min_base
-                    result_4[num].extend(t for t in d_path)
-                    result_4[num].extend(t for t in min_b_path)
-                    sequence.answer.remove(d_path)
-                    diamond_cost.clear()
-                    bases_cost.clear()
+            sequence = Minimax_algorithm(diamond_pos_4, required, pos_A)
+
+            all_agents = len(turn_data.agent_data)
+            agent_num = 0
+            for i in range(len(turn_data.agent_data)):
+                if turn_data.agent_data[i].name == self.name:
+                    agent_num = i
+                    break
+
+            my_diamonds = list()
+            dia_pos = 0
+            while agent_num + dia_pos * all_agents < len(sequence.answer):
+                my_diamonds.append(sequence.answer[agent_num + dia_pos * all_agents])
+                dia_pos += 1
+
+            origin_pos = turn_data.agent_data[agent_num].position
+            for diamond in my_diamonds:
+                algorithm = A_star(self.grid_size, origin_pos, diamond, turn_data.map)
+                d_path, d_cost = algorithm.solution(origin_pos)
+                for base in bases_pos:
+                    algorithm = A_star(self.grid_size, d_path, base, turn_data.map)
+                    b_path, b_cost = algorithm.solution(d_path)
+                    bases_cost[base] = [b_path, b_cost]
+                min_base = min(bases_cost.keys(), key=lambda t: bases_cost[t][1])
+                min_b_path = bases_cost[min_base][0]
+                origin_pos = min_base
+                result_4.extend(t for t in d_path)
+                result_4.extend(t for t in min_b_path)
+                sequence.answer.remove(d_path)
+                diamond_cost.clear()
+                bases_cost.clear()
         # ------------------------------------------ PHASE 4 ------------------------------------------
 
         """if turn_counter == 0:
@@ -85,7 +102,6 @@ class Agent(BaseAgent):
                     elif re.match(r'[a-z]', turn_data.map[map_row][map_column]):
                         bases_pos.append((map_row, map_column))
             turn_counter += 1
-
             for agent in turn_data.agent_data:
                 origin_pos = agent.position
                 while diamond_pos:
@@ -109,8 +125,7 @@ class Agent(BaseAgent):
                     bases_cost.clear()"""
 
         print(result_4)
-        current_act = result_4[agent_turn].pop(0)
-        agent_turn = (agent_turn + 1) % len(turn_data.agent_data)
+        current_act = result_4.pop(0)
 
         if current_act == "UP":
             return Action.UP
